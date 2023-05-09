835db050f251adf7cf8028e4c125a25d4459a625
iam-phenomenal <ayandiranibrahim@outlook.com>
2023-05-09 17:04:08 +0100

    Database integration

--- backend/config/app.js
+++ backend/config/app.js
@@ -1,7 +1,20 @@
+// Imports ...
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
 const { json, urlencoded } = require("express");
 
+const { db } = require("./database");
+
 const app = express();
+
+db.once("open", () => console.log("Database connected"));
+db.on("error", (error) => console.error(error));
+
+app.use(cors())
+app.use(morgan("common"))
+app.use(helmet())
 
 app.use(json());
 app.use(urlencoded({ extended: false }));
